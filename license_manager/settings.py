99c15bc2e1e68248d2297b3f9ec80ec775fb2ad9
Amey Kasbe <amey.kasbe@gmail.com>
2023-11-03 17:31:32 -0500

    Setup api app

--- license_manager/settings.py
+++ license_manager/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "users.apps.UsersConfig",
+    "api.apps.ApiConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
