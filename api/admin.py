c847433595b219fd49e9c87f38c5070588b9c9cd
Amey Kasbe <amey.kasbe@gmail.com>
2023-11-06 17:34:33 -0600

    Add models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Software)
+admin.site.register(License)
+admin.site.register(Recipient)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Software(models.Model):
+    name = models.CharField(max_length = 100)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+class Recipient(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+class License(models.Model):
+    INDIVIDUAL = "indiv"
+    GROUP = "grp"
+    LICENSE_TYPE_CHOICES =  ((INDIVIDUAL, "Individual"), (GROUP, "Group"))
+
+    software = models.ForeignKey(Software, on_delete=models.RESTRICT)
+    type = models.CharField(max_length=50, choices=LICENSE_TYPE_CHOICES, default=INDIVIDUAL)
+    start_date = models.DateField()
+    expiration_date = models.DateField()
+    recipients = models.ManyToManyField(Recipient)
+    max_users = models.IntegerField(default=1)
+
+    def __str__(self) -> str:
+        return self.software.name + ": " + dict(self.LICENSE_TYPE_CHOICES).get(self.type)
